628f4dfeabdaad5c7491a2a1457a98b3b8d73858
omonmunduz <omurbekn@gmail.com>
2021-02-26 16:53:19 -0800

    initial commit

---
+++ src/components/shared/Nav.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+
+import  BurgerIcon  from '../../assets/square.svg';
+
+
+const Nav = () => {
+    return <h1>Nav</h1>
+};
+export default Nav;
