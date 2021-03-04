b32c09cd51432f7715e61e1a4967177e62fba1e3
omonmunduz <omurbekn@gmail.com>
2021-03-04 09:25:38 -0800

    homepage layout edited

--- src/components/home/HomeLanding.js
+++ src/components/home/HomeLanding.js
@@ -10,9 +10,9 @@ import paymentss from './images/paymentss.svg';
 const HomeLanding = () => {
     return <section className="home-intro-section">
         <div className="intro-box">
-            <img src={addSimcard} alt="Add SIM card" className="intro-box-img"/>
+        <span className="order">1</span><img src={addSimcard} alt="Add SIM card" className="intro-box-img"/>
             <div className="intro-box-text">
-                <h3>Add SIM</h3>
+                <h4>Add SIM</h4>
                 <p className="intro-box-para">Lorem ipsum dolor sit amet,
                  consectetur adipiscing elit. Duis finibus at tellus vel ullamcorper. 
                  Maecenas vulputate ut diam ac cursus. Class aptent taciti sociosqu ad litora
@@ -21,8 +21,10 @@ const HomeLanding = () => {
             </div>
         </div>
         <div className="intro-box">
+        <span className="order">1</span>
+        <img src={destination} alt="Destination" className="intro-box-img"/>
         <div className="intro-box-text">
-                <h3>Select Desired Region and Country</h3>
+                <h4>Select Desired Region and Country</h4>
                 <p className="intro-box-para">After adding you SIM number go to <Link to="/store">Store</Link>
                  page where you can select you region
                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
@@ -30,12 +32,11 @@ const HomeLanding = () => {
                  Class aptent taciti sociosqu ad litora torquent per conubia <Link to="/store">Store</Link>
                    </p>
             </div>
-            <img src={destination} alt="Destination" className="intro-box-img"/>
         </div>
         <div className="intro-box">
-            <img src={choose} alt="Choose" className="intro-box-img"/>
+        <span className="order">3</span><img src={choose} alt="Choose" className="intro-box-img"/>
             <div className="intro-box-text">
-                <h2>Select Desired Package</h2>
+                <h4>Select Desired Package</h4>
                 <p className="intro-box-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Duis finibus at tellus vel ullamcorper. Maecenas vulputate ut diam ac cursus. 
                 Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
@@ -43,15 +44,15 @@ const HomeLanding = () => {
             </div>
         </div>
         <div className="intro-box">
+        <span className="order">4</span><img src={paymentss} alt="Payments" className="intro-box-img"/>
         <div className="intro-box-text">
-                <h2>Complete Payments</h2>
+                <h4>Complete Payments</h4>
                 <p className="intro-box-para">Lorem ipsum dolor sit amet, 
                 consectetur adipiscing elit. Duis finibus at tellus vel ullamcorper. 
                 Maecenas vulputate ut diam ac cursus. Class aptent taciti sociosqu ad 
                 litora torquent per conubia nostra, per inceptos himenaeos. 
                 Sed tellus arcu, porta sed consectetur ut, interdum at lacus</p>
             </div>
-            <img src={paymentss} alt="Payments" className="intro-box-img"/>
         </div>
     </section>
 };
